32c5beb9f10f95b65d2c09a958810162558e4c28
ZyrT12 <Zyryanova554@gmail.com>
2025-07-29 10:44:16 +0500

    Task№4

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 @Command(
         name = "gendiff",
@@ -12,6 +13,15 @@ import picocli.CommandLine.Option;
 )
 public class App implements Runnable {
 
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
     public static void main(String[] args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
@@ -19,6 +29,7 @@ public class App implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("Specify options or use -h for help.");
+        // Пока просто выводим аргументы
+        System.out.printf("Comparing files: %s and %s with format: %s%n", filepath1, filepath2, format);
     }
 }
